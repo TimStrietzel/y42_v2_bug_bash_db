2eac8dbc5804bc33306e79eba982879366e375cc
Talha Akber <ta@y42.com>
2022-08-11 14:14:57 +0200

    [Y42]: automatic migration of branch "main"

--- src/Models/SQL/test_sql/table_1/sql-model.sql
+++ src/Models/SQL/test_sql/table_1/sql-model.sql
@@ -2,4 +2,4 @@ Select
   order_date,
   last_click_channel,
   adspend
-from {{ ref('src/Integrations/mock_data/demo_amazon_ads.csv') }}
+from {{ ref('src/Integrations/mock_data/demo_amazon_ads.csv') }} limit 10
